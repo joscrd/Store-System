71c74d254eb682e4003e104165fc545d24c33594
joscrd <j194cc@gmail.com>
2024-05-30 03:02:09 -0700

    Add item modal and React-hook-form

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -1,14 +1,24 @@
 import CardStore from "./CardStore";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Modal, Form } from "react-bootstrap";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Stock() {
+    const [showModalCreate, setShowModalCreate] = useState(false);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
+    const handleCreate = () => {
+        setShowModalCreate(true);
+    }
 
+    const onSubmit = () => {
+        console.log('submit')
+    }
 
     return (
         <>
             <div className='addItemBtn d-flex justify-content-center'>
-                <Button>Add Item</Button>
+                <Button onClick={() => {handleCreate()}}>Add Item</Button>
             </div>
 
             <Container className="d-flex justify-content-center">
@@ -20,6 +30,27 @@ function Stock() {
                     ))}
                 </Row>
             </Container>    
+            
+            {/*CREATE ITEM MODAL*/}
+            <Modal show={showModalCreate} >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Item</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <Form onSubmit={handleSubmit(onSubmit)} >
+                        <Form.Group controlId="itemName">
+                            <Form.Label>Item Name</Form.Label>
+                            <Form.Control
+                            type="string"
+                            placeholder="Enter Item Name"
+                            {...register('itemName', { required: 'Item name is required' })}
+                            />
+                            {errors.itemName && <p className="text-danger">{errors.itemName.message}</p>}
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+            </Modal>
         </>
     );
 }
